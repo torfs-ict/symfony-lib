52256e470e12d3c86f00a35f65280ef70cd719e6
Kristof Torfs <kristof@torfs.org>
2018-10-16 09:34:52 +0200

    Added IdTrait

---
+++ src/Entity/Traits/IdTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace TorfsICT\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class IdTrait
+{
+    /**
+     * @var integer
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
