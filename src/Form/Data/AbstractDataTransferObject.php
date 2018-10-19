41aefb0078f18928c0d1b52a2bc25fe42b7b8b8b
Kristof Torfs <kristof@torfs.org>
2018-10-19 20:46:37 +0200

    Added simple abstract DTO

---
+++ src/Form/Data/AbstractDataTransferObject.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace TorfsICT\Form\Data;
+
+use ReflectionClass;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use TorfsICT\Entity\EntityInterface;
+
+abstract class AbstractDataTransferObject implements DataTransferObjectInterface
+{
+    final public function __construct()
+    {
+    }
+
+    /**
+     * @param EntityInterface|DataTransferObjectInterface $src
+     * @param EntityInterface|DataTransferObjectInterface $dst
+     * @return $this
+     */
+    private function sync($src, $dst): self
+    {
+        $accessor = PropertyAccess::createPropertyAccessor();
+        $class = new ReflectionClass($this);
+        foreach($class->getProperties() as $property) {
+            $property = $property->getName();
+            if (!$accessor->isReadable($src, $property)) continue;
+            if (!$accessor->isWritable($dst, $property)) continue;
+            $value = $accessor->getValue($src, $property);
+            $accessor->setValue($dst, $property, $value);
+        }
+        return $dst;
+    }
+
+    public function fromEntity(EntityInterface $entity): DataTransferObjectInterface
+    {
+        return $this->sync($entity, $this);
+    }
+
+    public function toEntity(EntityInterface $entity): EntityInterface
+    {
+        return $this->sync($this, $entity);
+    }
+}
